a07edd14de1ae36289405d7398e9f2b19491cbab
stephenbawks <stephen@bawks.com>
2022-11-05 10:04:59 -0400

    asdf

--- main.py
+++ main.py
@@ -156,7 +156,83 @@ async def read_item(ticker_symbol):
         }
         return Response(status_code=404, media_type="application/json", content=json.dumps(missing_ticker))
 
-    return yahoo_ticker_response
+    ticker_details = yahoo_ticker_response.get("quoteSummary").get("result")[0]
+    asset_profile = ticker_details.get("assetProfile") or {"assetProfile": {}}
+    financial_details = ticker_details.get("financialData") or {"financialData": {}}
+    default_stats = ticker_details.get("defaultKeyStatistics") or {"defaultKeyStatistics": {}}
+    summary_detail = ticker_details.get("summaryDetail") or {"summaryDetail": {}}
+    price_detail = ticker_details.get("price") or {"price": {}}
+
+    body = {
+        "ticker": ticker_upper,
+        "shortName": price_detail.get("shortName") or None,
+        "industry": asset_profile.get("industry") or None,
+        "sector": asset_profile.get("sector") or None,
+        "address1": asset_profile.get("address1") or None,
+        "city": asset_profile.get("city") or None,
+        "state": asset_profile.get("state") or None,
+        "zipCode": asset_profile.get("zip") or None,
+        "country": asset_profile.get("country") or None,
+        "phone": asset_profile.get("phone") or None,
+        "website": asset_profile.get("website") or None,
+        "fiftyTwoWeekHigh": summary_detail.get("fiftyTwoWeekHigh").get("raw") or None,
+        "fiftyTwoWeekLow": summary_detail.get("fiftyTwoWeekLow").get("raw") or None,
+        "averageDailyVolume10Day": divide_by_1000(value=price_detail.get("averageDailyVolume10Day", {"averageDailyVolume10Day": {}}).get("raw")) or None,
+        "averageDailyVolume3Month": divide_by_1000(value=price_detail.get("averageDailyVolume3Month", {"averageDailyVolume3Month": {}}).get("raw")) or None,
+        "marketCap": divide_by_1000000(value=summary_detail.get("marketCap", {"marketCap": {}}).get("raw")) or None,
+        "forwardPE":summary_detail.get("forwardPE", {"forwardPE": {}}).get("raw") or None,
+        "trailingPE": summary_detail.get("trailingPE", {"trailingPE": {}}).get("raw") or None,
+        "fullExchangeName":price_detail.get("exchangeName") or None,
+        "currentPrice": summary_detail.get("previousClose", {"previousClose": {}}).get("raw") or None,
+        "totalCashPerShare": financial_details.get("totalCashPerShare", {"totalCashPerShare": {}}).get("raw") or None,
+        "operatingMargins": multiply_by_100(financial_details.get("operatingMargins", {"operatingMargins": {}}).get("raw")) or None,
+        "freeCashflow": divide_by_1000000(value=financial_details.get("freeCashflow", {"freeCashflow": {}}).get("raw")) or None,
+        "grossMargins": multiply_by_100(value=financial_details.get("grossMargins", {"grossMargins": {}}).get("raw")) or None,
+        "ebitda": divide_by_1000000(value=financial_details.get("ebitda", {"ebitda": {}}).get("raw")) or None,
+        "totalDebt": divide_by_1000000(value=financial_details.get("totalDebt", {"totalDebt": {}}).get("raw")) or None,
+        "debtToEquity":financial_details.get("debtToEquity", {"debtToEquity": {}}).get("raw") or None,
+        "currentRatio": financial_details.get("currentRatio", {"currentRatio": {}}).get("raw") or None,
+        "totalRevenue": divide_by_1000000(value=financial_details.get("totalRevenue", {"totalRevenue": {}}).get("raw")) or None,
+        "profitMargins": multiply_by_100(value=financial_details.get("profitMargins", {"profitMargins": {}}).get("raw")) or None,
+        "targetHighPrice":financial_details.get("targetHighPrice", {"targetHighPrice": {}}).get("raw") or None,
+        "targetLowPrice": financial_details.get("targetLowPrice", {"targetLowPrice": {}}).get("raw") or None,
+        "targetMeanPrice": financial_details.get("targetMeanPrice", {"targetMeanPrice": {}}).get("raw") or None,
+        "targetMedianPrice": financial_details.get("targetMedianPrice", {"targetMedianPrice": {}}).get("raw") or None,
+        "recommendationMean": financial_details.get("recommendationMean", {"recommendationMean": {}}).get("raw") or None,
+        "recommendationKey": financial_details.get("recommendationKey") or None,
+        "numberOfAnalystOpinions": financial_details.get("numberOfAnalystOpinions", {"numberOfAnalystOpinions": {}}).get("raw") or None,
+        "returnOnAssets": financial_details.get("returnOnAssets", {"returnOnAssets": {}}).get("raw") or None,
+        "operatingCashflow": financial_details.get("operatingCashflow", {"operatingCashflow": {}}).get("raw") or None,
+        "earningsGrowth": multiply_by_100(value=financial_details.get("earningsGrowth", {"earningsGrowth": {}}).get("raw")) or None,
+        "revenueGrowth": multiply_by_100(value=financial_details.get("revenueGrowth", {"revenueGrowth": {}}).get("raw")) or None,
+        "ebitdaMargins": divide_by_1000000(value=financial_details.get("ebitdaMargins", {"ebitdaMargins": {}}).get("raw")) or None,
+        "fiftytwoWeekChange":default_stats.get("52WeekChange", {"52WeekChange": {}}).get("raw") or None,
+        "floatShares": divide_by_1000000(value=default_stats.get("floatShares", {"floatShares": {}}).get("raw")) or None,
+        "priceToBook": default_stats.get("priceToBook", {"priceToBook": {}}).get("raw") or None,
+        "sharesShort": divide_by_1000000(value=default_stats.get("sharesShort", {"sharesShort": {}}).get("raw")) or None,
+        "shortRatio": default_stats.get("shortRatio", {"shortRatio": {}}).get("raw") or None,
+        "shortPercentOfFloat": multiply_by_100(value=default_stats.get("shortPercentOfFloat", {"shortPercentOfFloat": {}}).get("raw")) or None,
+        "heldPercentInsiders": multiply_by_100(value=default_stats.get("heldPercentInsiders", {"heldPercentInsiders": {}}).get("raw")) or None,
+        "heldPercentInstitutions": multiply_by_100(value=default_stats.get("heldPercentInstitutions", {"heldPercentInstitutions": {}}).get("raw")) or None,
+        "lastFiscalYearEnd": default_stats.get("lastFiscalYearEnd", {"lastFiscalYearEnd": {}}).get("fmt") or None,
+        "forwardEps": default_stats.get("forwardEps", {"forwardEps": {}}).get("raw") or None,
+        "enterpriseToEbitda": default_stats.get("enterpriseToEbitda", {"enterpriseToEbitda": {}}).get("raw") or None,
+        "enterpriseToRevenue": default_stats.get("enterpriseToRevenue", {"enterpriseToRevenue": {}}).get("raw") or None,
+        "trailingEps": default_stats.get("trailingEps", {"trailingEps": {}}).get("raw") or None,
+        "lastSplitDate": default_stats.get("lastSplitDate", {"lastSplitDate": {}}).get("fmt") or None,
+        "lastSplitFactor": default_stats.get("lastSplitFactor") or None,
+        "sharesShortPriorMonth": divide_by_1000000(value=default_stats.get("sharesShortPriorMonth", {"sharesShortPriorMonth": {}}).get("raw")) or None,
+        "enterpriseValue": default_stats.get("enterpriseValue", {"enterpriseValue": {}}).get("raw") or None,
+        "earningsQuarterlyGrowth": multiply_by_100(value=default_stats.get("earningsQuarterlyGrowth", {"earningsQuarterlyGrowth": {}}).get("raw")) or None,
+        "revenueQuarterlyGrowth": multiply_by_100(value=default_stats.get("revenueQuarterlyGrowth", {"revenueQuarterlyGrowth": {}}).get("raw")) or None,
+        "mostRecentQuarter": default_stats.get("mostRecentQuarter", {"mostRecentQuarter": {}}).get("fmt") or None,
+        "pegRatio": default_stats.get("pegRatio", {"pegRatio": {}}).get("raw") or None,
+        "beta": summary_detail.get("beta", {"beta": {}}).get("raw") or None,
+        "priceToSalesTrailing12Months": summary_detail.get("priceToSalesTrailing12Months", {"priceToSalesTrailing12Months": {}}).get("raw") or None,
+        "dividendYield": multiply_by_100(value=summary_detail.get("dividendYield", {"dividendYield": {}}).get("raw")) or None
+    }
+
+    return Response(status_code=200, media_type="application/json", content=json.dumps(body))
 
 @app.get("/", status_code=200)
 async def root():
