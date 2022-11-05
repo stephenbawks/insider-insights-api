72cca6f97bd764bf0f1617e1c013a3a1f021a7ec
stephenbawks <stephen@bawks.com>
2022-11-05 09:05:54 -0400

    asdf

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-from fastapi import FastAPI, status, Response, HTTPException
+"""
+API
+"""
+
+from .helper_functions import (
+    multiply_by_100,
+    divide_by_1000,
+    divide_by_1000000,
+    convert_datetime
+)
+
 import pathlib
-from functools import lru_cache
 import json
+from functools import lru_cache
+
 from datetime import date, datetime, timedelta
 from functools import lru_cache
 from typing import List, Optional, Union
+
+from fastapi import FastAPI, status, Response, HTTPException
+
+
 
 
 app = FastAPI()
 
 
+# def query_yahoo_quote_summary(ticker_symbol: str) -> Union[dict, None]:
+#     """
+#     Query the Yahoo Financial API to pull back financial details for a single ticker symbol
 
+#     Args:
+#         ticker_symbol (str): The ticker symbol to pull information about.
+#     Returns:
+#         dict: The financial data for the ticker
+#     """
+
+#     url = f"https://yfapi.net/v11/finance/quoteSummary/{ticker_symbol}"
+
+#     headers = {
+#         "X-API-KEY": finance_api_key,
+#         "accept": "application/json"
+#     }
+
+#     query_string = {"modules":"defaultKeyStatistics,assetProfile,summaryDetail,financialData,price,earnings,earningsHistory"}
+
+#     response = http.request('GET', url, headers=headers, fields=query_string)
+#     print(f"Response Status Code: {response.status}")
+
+#     print(response.data.decode('utf-8'))
+#     response_decoded = json.loads(response.data.decode('utf-8'))
+
+#     if response_decoded.get("quoteSummary").get("result") is None:
+#         print(response_decoded)
+#         return None
+
+#     return response_decoded
 
 
 @lru_cache()
@@ -26,11 +70,32 @@ def get_holiday_dates() -> List[dict]:
     return json.loads(file_contents)
 
 
-@lru_cache()
 @app.get("/holidays", status_code=200)
 async def is_holiday():
+    """
+    Get the list of holidays
+
+    Returns:
+        _type_: _description_
+    """
     return pathlib.Path("holidays.txt").read_text()
 
+
+# @app.get("/ticker/{ticker_symbol}", status_code=200)
+# async def read_item(ticker_symbol):
+
+#     ticker_upper = ticker_symbol.upper()
+
+#     yahoo_ticker_response = query_yahoo_quote_summary(ticker_symbol=ticker_upper)
+#     if yahoo_ticker_response is None:
+#         raise HTTPException(status_code=404, detail=body)
+
+#         body = {
+#             "ticker": ticker_upper,
+#             "error": "Cannot find ticker"
+#         }
+
+#     return json.dumps(body)
 
 @app.get("/", status_code=200)
 async def root():
